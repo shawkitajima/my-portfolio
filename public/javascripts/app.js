1fdf45fb2103477616623e16bdb7791b962124b6
Shaw Kitajima <shawkitajima@gmail.com>
2020-01-21 12:07:47 -0800

    updates styling

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -2,6 +2,8 @@
 document.addEventListener('DOMContentLoaded', function() {
     var elems = document.querySelectorAll('.carousel');
     var instances = M.Carousel.init(elems);
+    var elems2 = document.querySelectorAll('.fixed-action-btn');
+    var instances2 = M.FloatingActionButton.init(elems2);
   });
 
 
